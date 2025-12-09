7a1031e72ba596dcd0a93e56afa3f399fca431e9
Maximilian Pohl <maximilian@tweedegolf.com>
2025-12-09 16:21:06 +0100

    Enforce newline at the end of every file

---
+++ .github/workflows/check_newline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ERRORS=0
+
+for f in $(git grep -Il '')
+do
+  if [[ $(tail -c 1 "$f") ]]; then
+    echo "$f"
+    ERRORS=1
+  fi
+done
+
+if [ $ERRORS -eq 1 ]; then
+  echo "Files above have no newline at the end."
+  exit 1
+fi
